a71d2a079cc9272cf5dac4e2c2f93503270da11d
takayuki <147408435+takayuki818@users.noreply.github.com>
2023-11-08 00:19:42 +0900

    Add files via upload

--- ダイアログからCSV読込.py
+++ ダイアログからCSV読込.py
@@ -4,12 +4,6 @@ import PySimpleGUI as sg
 from tkinter import filedialog
 import sys
 def CSV読込(file_path,header_rows):
-    if file_path==''or file_path==None:
-        sg.popup('読込CSVファイルがありません')
-        sys.exit()
-    if not isinstance(header_rows,int):
-        sg.popup('見出し行数が整数ではありません')
-        sys.exit()
     output_excel_path='CSV展開.xlsx'
     encoding='utf-8'
     delimiter=','
@@ -25,6 +19,13 @@ def CSV読込(file_path,header_rows):
                 sh.cell(row=row_idx-header_rows+1,column=col_idx+1,value=value)
     wb.save(output_excel_path)
     wb.close
+def 整数判定(value):
+    try:
+        int(value, 10) 
+    except ValueError:
+        return False
+    else:
+        return True
 layout=[[sg.Text('展開するCSVファイルのパスを入力（Browseから選択）')], 
         [sg.InputText(),sg.FileBrowse(key='file_path', file_types=(('CSVファイル', '*.csv'),))],
         [sg.Text('読込から除外する見出し行数を入力（全行読込の場合は「0」）'),sg.InputText(key='header_rows',size=(5,))],
@@ -36,8 +37,15 @@ while True:
         break
     elif event=='OK':
         file_path=values['file_path']
-        header_rows=int(values['header_rows'])
-        CSV読込(file_path,header_rows)
-        sg.popup('CSVファイル読込が完了しました')
-        break
+        header_rows=values['header_rows']
+        if 整数判定(header_rows):
+            if file_path=='':
+                sg.popup('CSVファイルのパスを入力してください')
+            else:
+                header_rows=int(values['header_rows'])
+                CSV読込(file_path,header_rows)
+                sg.popup('CSVファイル読込が完了しました')
+                break
+        else:
+            sg.popup('見出し行数には整数を入力してください')
 window.close()
