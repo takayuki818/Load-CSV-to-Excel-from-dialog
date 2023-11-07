157ca6af009a0bb58e57dcd207ef5a4263b142ce
takayuki <147408435+takayuki818@users.noreply.github.com>
2023-11-07 23:51:52 +0900

    Add files via upload

--- ダイアログからCSV読込.py
+++ ダイアログからCSV読込.py
@@ -1,22 +1,19 @@
 import openpyxl
 import csv
-import tkinter as tk
+import PySimpleGUI as sg
 from tkinter import filedialog
 import sys
-def パス取得():
-    root=tk.Tk()
-    root.withdraw()
-    file_path=filedialog.askopenfilename()
-    return file_path
-def CSV読込(file_path):
-    if file_path=='':
-        print('読込CSVファイルがありません')
+def CSV読込(file_path,header_rows):
+    if file_path==''or file_path==None:
+        sg.popup('読込CSVファイルがありません')
+        sys.exit()
+    if not isinstance(header_rows,int):
+        sg.popup('見出し行数が整数ではありません')
         sys.exit()
     output_excel_path='CSV展開.xlsx'
     encoding='utf-8'
     delimiter=','
     newline=''
-    header_rows=1
     wb=openpyxl.Workbook()
     sh=wb.active
     with open(file_path,'r',encoding=encoding,newline=newline) as csv_file:
@@ -28,4 +25,19 @@ def CSV読込(file_path):
                 sh.cell(row=row_idx-header_rows+1,column=col_idx+1,value=value)
     wb.save(output_excel_path)
     wb.close
-CSV読込(パス取得())
+layout=[[sg.Text('展開するCSVファイルのパスを入力（Browseから選択）')], 
+        [sg.InputText(),sg.FileBrowse(key='file_path', file_types=(('CSVファイル', '*.csv'),))],
+        [sg.Text('読込から除外する見出し行数を入力（全行読込の場合は「0」）'),sg.InputText(key='header_rows',size=(5,))],
+        [sg.Button('CSV読込実行',key='OK')]]
+window=sg.Window('CSV読込設定',layout)
+while True:
+    event,values=window.read()
+    if event==sg.WIN_CLOSED:
+        break
+    elif event=='OK':
+        file_path=values['file_path']
+        header_rows=int(values['header_rows'])
+        CSV読込(file_path,header_rows)
+        sg.popup('CSVファイル読込が完了しました')
+        break
+window.close()
