58753a60efbf957d714e8274f2a4eeb4bd158926
takayuki <147408435+takayuki818@users.noreply.github.com>
2023-11-14 23:58:41 +0900

    Update ダイアログからCSV読込.py

--- ダイアログからCSV読込.py
+++ ダイアログからCSV読込.py
@@ -22,30 +22,33 @@ def 整数判定(value):
         return False
     else:
         return True
-layout=[[sg.Text('展開するCSVファイルのパスを入力（Browseから選択）')], 
-        [sg.InputText(),sg.FileBrowse(key='file_path', file_types=(('CSVファイル', '*.csv'),))],
-        [sg.Text('読込除外見出行数（全行読込の場合は「0」）'),sg.InputText(key='header_rows',size=(5,))],
-        [sg.Text('文字コードを選択'),sg.Combo(['UTF-8','SHIFT-JIS','CP932'],default_value='UTF-8',size=(6,1), key='encoding'),
-         sg.Text('区切り文字を入力'),sg.InputText(',',justification='center',size=(5,),key='delimiter')],
-        [sg.Button('CSV読込実行',key='OK')]]
-window=sg.Window('CSV読込設定',layout)
-while True:
-    event,values=window.read()
-    if event==sg.WIN_CLOSED:
-        break
-    elif event=='OK':
-        file_path=values['file_path']
-        header_rows=values['header_rows']
-        encoding=values['encoding']
-        delimiter=values['delimiter']
-        if 整数判定(header_rows):
-            if file_path=='':
-                sg.popup('CSVファイルのパスを入力してください')
+def main():
+    layout=[[sg.Text('展開するCSVファイルのパスを入力（Browseから選択）')], 
+            [sg.InputText(),sg.FileBrowse(key='file_path', file_types=(('CSVファイル', '*.csv'),))],
+            [sg.Text('読込除外見出行数（全行読込の場合は「0」）'),sg.InputText(key='header_rows',size=(5,))],
+            [sg.Text('文字コードを選択'),sg.Combo(['UTF-8','SHIFT-JIS','CP932'],default_value='UTF-8',size=(6,1), key='encoding'),
+             sg.Text('区切り文字を入力'),sg.InputText(',',justification='center',size=(5,),key='delimiter')],
+            [sg.Button('CSV読込実行',key='OK')]]
+    window=sg.Window('CSV読込設定',layout)
+    while True:
+        event,values=window.read()
+        if event==sg.WIN_CLOSED:
+            break
+        elif event=='OK':
+            file_path=values['file_path']
+            header_rows=values['header_rows']
+            encoding=values['encoding']
+            delimiter=values['delimiter']
+            if 整数判定(header_rows):
+                if file_path=='':
+                    sg.popup('CSVファイルのパスを入力してください')
+                else:
+                    header_rows=int(values['header_rows'])
+                    CSV読込(file_path,header_rows,encoding,delimiter)
+                    sg.popup('CSVファイル読込が完了しました')
+                    break
             else:
-                header_rows=int(values['header_rows'])
-                CSV読込(file_path,header_rows,encoding,delimiter)
-                sg.popup('CSVファイル読込が完了しました')
-                break
-        else:
-            sg.popup('見出し行数には整数を入力してください')
-window.close()
+                sg.popup('見出し行数には整数を入力してください')
+    window.close()
+if __name__ == '__main__':
+    main()
